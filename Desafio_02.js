18edcedfc5cd3cd6aea3f6c2c79bf4ed54829f56
Renato Santos <renato_jfs@hotmail.com>
2022-04-22 00:09:27 -0300

    second challenge

---
+++ Desafio_02.js
@@ -0,0 +1,12 @@
+// Escreva uma função que recebe um parâmetro e retorna o tipo do valor recebido nesse parâmetro.
+
+// Resultado esperado:
+// myFunction(1) // number
+// myFunction(false) // boolean
+// myFunction({}) // object
+// myFunction("Hello World") // string
+// myFunction(["Hello World"]) // object
+
+function myFunction(a) {
+  return typeof a;
+}
